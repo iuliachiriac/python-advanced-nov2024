d5d14bf5279a716f7203f36f8ab3338e232a1594
Iulia Chiriac <iulia.chiriac@yahoo.com>
2024-11-18 17:13:03 +0200

    Fix is_prime and add solutions for else in loops

--- solutions/ex_2_2_1_builtin_types_numbers.py
+++ solutions/ex_2_2_1_builtin_types_numbers.py
@@ -6,7 +6,9 @@ def is_prime(nr):
         return False
     if nr == 2 or nr == 3:
         return True
-    for i in range(3, int(nr ** 0.5), 2):
+    if nr % 2 == 0:
+        return False
+    for i in range(3, int(nr ** 0.5) + 1, 2):
         if nr % i == 0:
             return False
     return True
@@ -16,3 +18,4 @@ print(is_prime(100))
 print(is_prime(17))
 print(is_prime(57))
 print(is_prime(97))
+print(is_prime(8))
