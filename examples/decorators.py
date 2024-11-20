5cf61976ba5b734bde3c9a707dabe0533443943b
Iulia Chiriac <iulia.chiriac@yahoo.com>
2024-11-20 12:02:02 +0200

    Add decorators example

---
+++ examples/decorators.py
@@ -0,0 +1,32 @@
+def make_pretty(func):
+    def inner(*args, **kwargs):
+        print(f"I am {func.__name__} and I got decorated")
+        output = func(*args, **kwargs)
+        print()
+        return output
+    return inner
+
+
+@make_pretty
+def ordinary():
+    print("I am ordinary")
+# ordinary = make_pretty(ordinary)  # ordinary = make_pretty.<locals>.inner
+
+
+@make_pretty
+def greet(name):
+    print(f"Hello, {name}!")
+
+
+@make_pretty
+def compute(a, b, c=0):
+    return a * b + c
+
+
+ordinary()
+
+greet("Anna")
+greet(name="John")
+
+result = compute(20, 4, 4)
+print("computation result:", result)
