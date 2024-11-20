8d6f4e3f6eba3d5396ce2ece4d6ebfb3c367b4be
Iulia Chiriac <iulia.chiriac@yahoo.com>
2024-11-20 16:50:43 +0200

    Add property example

--- examples/oop.py
+++ examples/oop.py
@@ -3,24 +3,39 @@ from datetime import date
 
 class Person:
     count = 0  # class attributes
+    MIN_YEAR = 1900
 
-    def __init__(self, name, date_of_birth):
+    def __init__(self, name, date_of_birth: date):
         self.name = name  # instance attributes
         self.date_of_birth = date_of_birth
-        self.increment_count()
+        self._increment_count()
+
+    @property
+    def date_of_birth(self):
+        return self._date_of_birth
+
+    @date_of_birth.setter
+    def date_of_birth(self, value: date):
+        if value.year < self.MIN_YEAR:
+            raise ValueError("invalid date of birth")
+        self._date_of_birth = value
+
+    @property
+    def age(self):
+        return self.years_since(self._date_of_birth)
 
     def __str__(self):
         return f"<{__name__}.{self.__class__.__name__} name={self.name} "\
                f"date_of_birth={self.date_of_birth}>"
 
     def __lt__(self, other):
-        return self.date_of_birth > other.date_of_birth
+        return self._date_of_birth > other.date_of_birth
 
     def __le__(self, other):
-        return self.date_of_birth >= other.date_of_birth
+        return self._date_of_birth >= other.date_of_birth
 
     @classmethod
-    def increment_count(cls):
+    def _increment_count(cls):
         cls.count += 1
 
     @staticmethod
@@ -42,7 +57,12 @@ if __name__ == "__main__":
     p2 = Person("John", date(1987, 6, 23))
     print(p2.name)
 
-    print(str(p1.date_of_birth), repr(p1.date_of_birth))
+    try:
+        p2.date_of_birth = date(1887, 6, 23)
+    except ValueError as ex:
+        print(ex)
+
+    print(str(p2.date_of_birth), repr(p1.date_of_birth))
 
     print(f"{p1.name} is younger than {p2.name}: {p1 < p2}")
 
@@ -51,3 +71,4 @@ if __name__ == "__main__":
     print("Person count:", Person.count)
 
     print(Person.years_since(date(1918, 12, 1)))
+    print(f"{p1.name} is {p1.age} years old.")
